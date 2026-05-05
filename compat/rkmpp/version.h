368505a9c29229112d20ff3c801cdc399a758cf9
Noxwell <beebono@yahoo.com>
2026-05-05 17:38:35 -0500

    locomo/zig changes

---
+++ compat/rkmpp/version.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010 Rockchip Electronics S.LSI Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define	MPP_VERSION             "dummy_ver"
+#define	MPP_VER_HIST_CNT        0
+#define	MPP_VER_HIST_0          "dummy_hist"
+#define	MPP_VER_HIST_1          "dummy_hist"
+#define	MPP_VER_HIST_2          "dummy_hist"
+#define	MPP_VER_HIST_3          "dummy_hist"
+#define	MPP_VER_HIST_4          "dummy_hist"
+#define	MPP_VER_HIST_5          "dummy_hist"
+#define	MPP_VER_HIST_6          "dummy_hist"
+#define	MPP_VER_HIST_7          "dummy_hist"
+#define	MPP_VER_HIST_8          "dummy_hist"
+#define	MPP_VER_HIST_9          "dummy_hist"
+
+#endif /*__VERSION_H__*/
